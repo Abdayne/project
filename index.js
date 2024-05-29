b52b039dfa8763c05729efeba3af00b2d3c41f43
Abdayne <olasunkamiabdurrahman@gmail.com>
2024-05-29 11:56:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+a= '234'
+console.log(a, typeof(a));
+ b = parseInt(a)
+ console.log(b, typeof(b));
+
+ let i=10
+ i++
+ console.log(i);
+ let v=3
+ console.log(++v);
+ let result1= i / v
+ let result2= i % v
+ let result3= i <= v
+
+ console.log(result1,result2,result3);
+let x=1
+let y=2
+let z=3
+console.log(x<y&&y>z);
